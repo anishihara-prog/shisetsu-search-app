5899d8a0cc4b0644ead5a0505749a2383d2aa500
anishihara-prog <a.nishihara@wappa-no-kai.jp>
2026-04-03 11:37:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import re
 st.title("事業所一覧検索アプリ")
 
 # 常に最新取得
-FILE_URL = "https://drive.google.com/uc?export=download&id=FILE_ID"
+FILE_URL = "FILE_URL = "https://drive.google.com/uc?export=download&id=1caVKtJSJGkTq681J-fH6duvrOAHzY1uA"
+"
                                                                                 
 
 # =========================
